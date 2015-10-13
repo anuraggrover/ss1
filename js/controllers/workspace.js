bf554cf3356b2d273b9cd3112a02e520c2994b57
hemank-s <hemank@hike.in>
2015-10-13 17:34:30 +0530

    Home Screen Project X

--- js/controllers/workspace.js
+++ js/controllers/workspace.js
@@ -3,11 +3,11 @@
 
     var WorkspaceController = function (options) {
         this.template = require('raw!../../templates/workspace.html');
-    }
+    };
 
     WorkspaceController.prototype.render = function() {
         this.el = $(Mustache.render(this.template, {
-            balance: 1000
+            balance: 3000
         }));
 
         return this;
