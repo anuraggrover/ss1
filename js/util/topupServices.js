7d8144aacad10de28ceb22f5e634bf56460fcd34
Amit Verma <symbian.works@gmail.com>
2015-11-23 20:37:34 +0530

    Changed topupService endpoint

--- js/util/topupServices.js
+++ js/util/topupServices.js
@@ -9,7 +9,7 @@
     TopupService.prototype = {
         communicate: function (params, fn, x) {
             var that = this,
-                requestUrl = 'http://172.16.3.20:8080/hike-topup-service/' + params.url,
+                requestUrl = 'http://projectx-staging.hike.in/hike-topup-service/' + params.url,
                 startTime = Date.now(),
                 endTime;
 
